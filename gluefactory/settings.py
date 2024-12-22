fa01a2227aaf7990e5120b93181ced4f1d99b1cc
Yalanskyi Roman <keito.eternal@gmail.com>
2024-12-22 17:29:32 +0200

    path bug fix

--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-DATA_PATH = "/kaggle/input/"  # datasets and pretrained weights
-TRAINING_PATH = "/kaggle/working/outputs/training/"  # training checkpoints
-EVAL_PATH = "/kaggle/working/outputs/results/"  # evaluation results
+DATA_PATH = Path("/kaggle/input/")  # datasets and pretrained weights
+TRAINING_PATH = Path("/kaggle/working/outputs/training/")  # training checkpoints
+EVAL_PATH = Path("/kaggle/working/outputs/results/")  # evaluation results
